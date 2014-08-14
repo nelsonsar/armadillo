ac12ac9c4cc7d1598b702f947df4cb7d049032c8
Gabriel Schmitt <gabrielsch@outlook.com>
2014-08-13 21:56:11 -0300

    Adding 100% coverage to event dispatcher.

--- tests/Armadillo/EventDispatcherTest.php
+++ tests/Armadillo/EventDispatcherTest.php
@@ -14,4 +14,49 @@ class EventDispatcherTest extends \PHPUnit_Framework_TestCase
 
         $this->assertCount(1, $dispatcher->getRegisteredEvents());
     }
+
+    public function testAddingListenerToEvent()
+    {
+        $eventName = 'foo';
+        $listener = $this->getMock('\SplObserver');
+
+        $dispatcher = new EventDispatcher();
+        $dispatcher->registerEvent($eventName);
+        $dispatcher->addListenerToEvent($eventName, $listener);
+        $events = $dispatcher->getRegisteredEvents();
+        /** @var Event $event */
+        $event = $events[$eventName];
+
+        $this->assertTrue($event->getListeners()->contains($listener));
+    }
+
+    public function testRemovingListenerFromEvent()
+    {
+        $eventName = 'foo';
+        $listener = $this->getMock('\SplObserver');
+
+        $dispatcher = new EventDispatcher();
+        $dispatcher->registerEvent($eventName);
+        $dispatcher->addListenerToEvent($eventName, $listener);
+
+        $events = $dispatcher->getRegisteredEvents();
+        /** @var Event $event */
+        $event = $events[$eventName];
+        $listeners = $event->getListeners();
+
+        $this->assertTrue($listeners->contains($listener));
+
+        $dispatcher->removeListenerFromEvent($eventName, $listener);
+
+        $this->assertFalse($listeners->contains($listener));
+    }
+
+    public function testEventCanBeDispatched()
+    {
+        $eventName = 'foo';
+        $dispatcher = new EventDispatcher();
+        $dispatcher->registerEvent($eventName);
+        $data = ['foo', 'bar'];
+        $dispatcher->dispatchEvent($eventName, $data);
+    }
 }
